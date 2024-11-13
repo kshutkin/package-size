2f42fbc0e2880a1294f2bf3247780b183eafa2dd
Konstantin Shutkin <kostya80@gmail.com>
2024-11-13 22:12:34 +0100

    added test

--- test.js
+++ test.js
@@ -7,7 +7,58 @@ const exec = promisify(cd.exec);
 
 describe('pkgsz', () => {
     it('simple', async () => {
-        const { stdout } = await exec('node index.js @tstpkgs/with-legal-single-line-comment');
-        assert.equal(stdout, '');
+        const { stdout } = await exec('node index.js @tstpkgs/with-legal-single-line-comment -j');
+        assert.equal(clean(stdout), clean(`{
+            "metadata": {
+                "name": "@tstpkgs/with-legal-single-line-comment",
+                "version": "0.0.2"
+            },
+            "exports": [],
+            "includedExports": [
+                {
+                    "export": ".",
+                    "defaultExport": false
+                }
+            ],
+            "results": [
+                {
+                "id": "nodeModulesSize",
+                    "value": 1930,
+                    "unit": "bytes"
+                },
+                {
+                "id": "nodeModulesFiles",
+                    "value": 5,
+                    "unit": "count"
+                },
+                {
+                "id": "sizeMinified",
+                    "value": 51,
+                    "unit": "bytes"
+                },
+                {
+                    "id": "sizeMinifiedGzipped",
+                    "value": 71,
+                    "unit": "bytes"
+                }
+            ],
+            "composition": [
+                [
+                    "@tstpkgs/with-legal-single-line-comment",
+                    50
+                ],
+                [
+                    "[EOLs]",
+                    2
+                ]
+            ]
+        }`));
     });
 });
+
+/**
+ * @param {string} str 
+ */
+function clean(str) {
+    return str.replace(/\s+/g, ' ').trim();
+}
